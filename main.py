3c827a93dc21fd06d9606f2eb47f50fcffae2298
c0nn3r <connervercellino@gmail.com>
2017-02-28 23:05:37 -0800

    Guess you could call this an improvement.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
 import random
 import numpy as np
+import subprocess as sp
 from collections import namedtuple
 
-WIDTH, HEIGHT = 10, 10
+WIDTH, HEIGHT = 40, 40
 
 grid = np.full((WIDTH, HEIGHT), 'N', dtype=str)
 
@@ -10,7 +12,7 @@ point = namedtuple('Point', ['x', 'y'])
 
 
 def directions_iterator():
-    directions = ['R', 'L', 'D', 'U']
+    directions = ['→', '←', '↓', '↑']
 
     random.shuffle(directions)
 
@@ -18,21 +20,35 @@ def directions_iterator():
         yield each
 
 
-new_direction_x = {'R': 1, 'L': -1, 'D': 0, 'U': 0}
-new_direction_y = {'R': 0, 'L': 0, 'D': -1, 'U': 1}
+new_direction_x = {'→': 1, '←': -1, '↓': 0, '↑': 0}
+new_direction_y = {'→': 0, '←': 0, '↓': -1, '↑': 1}
 
-opposite_direction = {'R': 'L', 'L': 'R', 'U': 'D', 'D': 'U'}
+opposite_direction = {'→': '←', '←': '→', '↑': '↓', '↓': '↑'}
 
 
 def carve_passages_from(current_point, grid):
+    # print(grid)
+    # time.sleep(.01)
+    # _ = sp.call('clear', shell=True)
+
     for each in directions_iterator():
 
         new_point = point(current_point.x + new_direction_x[each],
                           current_point.y + new_direction_y[each])
 
         if 0 <= new_point.x <= (WIDTH - 1) and 0 <= new_point.y <= (HEIGHT - 1) and grid[new_point.x][new_point.y] == 'N':
-            grid[current_point.x][current_point.y] = each
-            grid[new_point.x][new_point.y] = opposite_direction[each]
+            if new_direction_x[each] == 1:
+                arrow = '→'
+            elif new_direction_x[each] == -1:
+                arrow = '←'
+            elif new_direction_y[each] == 1:
+                arrow = '↑'
+            elif new_direction_y[each] == -1:
+                arrow = '↓'
+            print(arrow)
+
+            grid[current_point.x][current_point.y] = arrow
+            grid[new_point.x][new_point.y] = opposite_direction[arrow]
 
             carve_passages_from(new_point, grid)
 
@@ -43,14 +59,14 @@ carve_passages_from(point(0, 0), grid)
 print(grid)
 
 for y in range(HEIGHT):
-    print('|')
+    print('|', end='')
     for x in range(WIDTH):
-        if grid[y][x] == 'R':
+        if grid[y][x] == '→':
+            print('```', end='')
+        elif grid[y][x] == '←':
             print('___', end='')
-        elif grid[y][x] == 'L':
-            print('___', end='')
-        elif grid[y][x] == 'U':
+        elif grid[y][x] == '↑':
             print('| |', end='')
-        elif grid[y][x] == 'D':
+        elif grid[y][x] == '↓':
             print('| |', end='')
-print('')
+    print('|')
