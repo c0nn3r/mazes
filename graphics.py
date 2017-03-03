dcd022d59cec37e9a59ca52da42f0f3116ef4daf
Austin Veseliza <auves99@icloud.com>
2017-03-03 14:56:21 -0800

    test svg file

--- graphics.py
+++ graphics.py
@@ -1,29 +1,5 @@
-"""
-import cairocffi as cairo
+import svgwrite as sw
 
-
-surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
-ctx = cairo.Context(surface)
-
-ctx.scale(WIDTH, HEIGHT)  # Normalizing the canvas
-
-ctx.rectangle(0, 0, 1, 1)  # Rectangle(x0, y0, x1, y1)
-ctx.fill()
-
-
-def draw_something():
-    ctx.move_to(.1, .5)
-    ctx.line_to(.1, .4)  # Line to (x,y)
-    ctx.rel_line_to(0.1, -0.1)
-    ctx.close_path()
-    ctx.set_source_rgb(1, 1, 1)  # Solid color
-    ctx.set_line_width(1)
-    ctx.stroke()
-
-
-for i in range(5):
-    draw_something()
-
-
-surface.write_to_png("example.png")  # Output to PNG
-"""
+svg = sw.Drawing(filename='test.svg', size=('100px','100px'))
+svg.add(svg.line((25,25), (75,75), stroke=sw.rgb(10,10,10,'%')))
+svg.save()
