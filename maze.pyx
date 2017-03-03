cf64a7f3d59c846b7347d6f02f5270c77732b923
c0nn3r <connervercellino@gmail.com>
2017-03-03 09:03:55 -0800

    Saving work before doing something risky.

--- maze.pyx
+++ maze.pyx
@@ -1,3 +1,5 @@
+'''#cython: boundscheck=False, wraparound=False, nonecheck=False'''
+
 import random
 cimport cython
 
@@ -13,7 +15,6 @@ from collections import namedtuple
 Point = namedtuple('Point', ['y', 'x'])
 
 
-@cython.boundscheck(False)
 def valid_point(point, np.ndarray[DTYPE_t, ndim=2] grid):
     if 0 <= point.x <= (grid.shape[1] - 1) and 0 <= point.y <= (grid.shape[0] - 1):
         if grid[point] == 0:
@@ -21,33 +22,32 @@ def valid_point(point, np.ndarray[DTYPE_t, ndim=2] grid):
     return False
 
 
-@cython.boundscheck(False)
 def check_surrounding(point, np.ndarray[DTYPE_t, ndim=2] grid):
-    random_directions = ['right', 'left', 'up', 'down']
+    random_directions = [1, 2, 3, 4]
 
     random.shuffle(random_directions)
 
     for each in random_directions:
 
-        if each is 'right':
+        if each == 1:
             new_point = Point(x=point.x + 1, y=point.y)
             if valid_point(new_point, grid):
-                return new_point, 'right'
+                return new_point, 1
 
-        elif each is 'left':
+        elif each == 2:
             new_point = Point(x=point.x - 1, y=point.y)
             if valid_point(new_point, grid):
-                return new_point, 'left'
+                return new_point, 2
 
-        elif each is 'up':
+        elif each == 3:
             new_point = Point(x=point.x, y=point.y - 1)
             if valid_point(new_point, grid):
-                return new_point, 'up'
+                return new_point, 3
 
-        elif each is 'down':
+        elif each == 4:
             new_point = Point(x=point.x, y=point.y + 1)
             if valid_point(new_point, grid):
-                return new_point, 'down'
+                return new_point, 4
 
     return False
 
@@ -57,7 +57,6 @@ def recursive_backtracker(starting_point, np.ndarray[DTYPE_t, ndim=2] grid):
     stack = []
     current_point = starting_point
     cdef int last_direction_char = 0
-    direction_to_char = {'right': 1, 'left': 2, 'up': 3, 'down': 4}
     backtracking = False
 
     while True:
@@ -71,7 +70,7 @@ def recursive_backtracker(starting_point, np.ndarray[DTYPE_t, ndim=2] grid):
 
             stack.append(current_point)
             new_point, new_direction_num = check_result
-            new_direction_char = direction_to_char[new_direction_num]
+            new_direction_char = new_direction_num
 
             grid[current_point] = last_direction_char
 
