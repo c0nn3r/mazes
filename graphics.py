9f2698d1f1dd903a693a1837648a897c4390af79
c0nn3r <connervercellino@gmail.com>
2017-03-07 11:46:21 -0800

    Working svg generator

--- graphics.py
+++
@@ -1,5 +0,0 @@
-import svgwrite as sw
-
-svg = sw.Drawing(filename='test.svg', size=('100px','100px'))
-svg.add(svg.line((25,25), (75,75), stroke=sw.rgb(10,10,10,'%')))
-svg.save()
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import time
 import random
 import numpy as np
+import svgwrite as sw
+
 from collections import namedtuple
 
-WIDTH = 30
-HEIGHT = 30
+WIDTH = 100
+HEIGHT = 100
 
 grid = np.full((HEIGHT, WIDTH), ' ', dtype=str)
 
 Point = namedtuple('Point', ['y', 'x'])
+
 
 
 def valid_point(point, grid):
@@ -33,6 +36,30 @@ def print_maze(grid):
             else:
                 print('|', end='')
         print('')
+
+
+def save_maze_to_svg(path, grid):
+    maze_height = 10
+    maze_width = 10
+
+    svg = sw.Drawing(filename=path, size=(str(WIDTH * maze_width + 100) + 'px', str(HEIGHT * maze_height + 100) + 'px'))
+    svg.add(svg.line((0.5, 1), (WIDTH * maze_width + 1.5, 1), stroke=sw.rgb(1, 1, 1, '%')))
+    svg.add(svg.line((1, 0.5), (1, HEIGHT * maze_height + 1.5), stroke=sw.rgb(1, 1, 1, '%')))
+
+    for y in range(HEIGHT):
+
+        for x in range(WIDTH):
+            if (y + 1 != HEIGHT and grid[y + 1, x] == 'v') or grid[y, x] == '^':
+                pass
+            else:
+                svg.add(svg.line((x * maze_width + 0.5, (y + 1) * maze_width + 1), ((x + 1) * maze_width + 1.5, (y + 1) * maze_height + 1), stroke=sw.rgb(1, 1, 1, '%')))
+            if (x + 1 != WIDTH and grid[y, x + 1] == '>') or grid[y, x] == '<':
+                pass
+            else:
+                svg.add(svg.line(((x + 1) * maze_width + 1, y * maze_height + 0.5), ((x + 1) * maze_width + 1, ((y + 1) * maze_height) + 1.5), stroke=sw.rgb(1, 1, 1, '%')))
+
+        # svg.add(svg.line((25, 25), (75, 75), stroke=sw.rgb(10, 10, 10, '%')))
+    svg.save()
 
 
 def check_surrounding(point, grid):
@@ -101,4 +128,4 @@ def recursive_backtracker(starting_point, grid):
 
 
 recursive_backtracker(Point(0, 0), grid)
-print_maze(grid)
+save_maze_to_svg('test.svg', grid)
